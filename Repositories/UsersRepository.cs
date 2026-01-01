5242be3305a8721872aa3e7a6c33dc43166d5432
Omer Haimovich <omer16493@gmail.com>
2026-01-01 16:50:55 +0200

    create users repository

---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace UsersService.Repositories
+{
+    public interface IUsersRepository
+    {
+
+    }
+
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public UsersRepository(IConfiguration config)
+        {
+            _connection = new SqlConnection(config.GetConnectionString("UsersDb"));
+        }
+    }
+}
