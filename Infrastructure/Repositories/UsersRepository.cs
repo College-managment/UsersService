dffd7688d3186ce7695531bc4b2aecb0178ce59e
Omer Haimovich <omer16493@gmail.com>
2026-01-18 16:43:16 +0200

    create users repository

--- Infrastructure/Repositories/UsersRepository.cs
+++ Infrastructure/Repositories/UsersRepository.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using System.Data;
+using UsersService.Domain.Entities;
 
 namespace UsersService.Infrastructure.Repositories
 {
@@ -11,21 +13,30 @@ namespace UsersService.Infrastructure.Repositories
 
     public class UsersRepository : IUsersRepository
     {
-        private readonly IDbConnection _connection;
+        private readonly string _connectionString;
+        private readonly ILogger<UsersRepository> _logger;
 
-        public UsersRepository(IConfiguration config)
+        public UsersRepository(IConfiguration config,
+                               ILogger<UsersRepository> logger)
         {
-            _connection = new SqlConnection(config.GetConnectionString("UsersDb"));
+            _connectionString = config.GetConnectionString("UsersDb") ?? throw new InvalidOperationException("UsersDb connection string not configured");
+            _logger = logger;
         }
+
+        private IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 
         Task<int> IUsersRepository.InsertUserAsync(string email, string passwordHash, CancellationToken ct)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> EmailExistsAsync(string email, CancellationToken ct)
+        public async Task<bool> EmailExistsAsync(string email, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            using var conn = CreateConnection();
+            var res = await conn.QueryAsync<List<User>>("usp_GetUserByEmail", new { Email = email }, commandType: CommandType.StoredProcedure);
+            if (res.Count() > 1)
+                return true;
+            return false;
         }
     }
 }
